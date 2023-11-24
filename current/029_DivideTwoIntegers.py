01f7fe464531957096aa03aca386de7a1ffb37b8
GondorGitaros <csaszardaniel81@gmail.com>
2023-11-25 00:02:16 +0100

    Starting To Solve A Problem

--- current/029_DivideTwoIntegers.py
+++ current/029_DivideTwoIntegers.py
@@ -2,32 +2,10 @@ dividend = int(input("Dividend: "))
 divisor = int(input("Divisor: "))
 
 def dividetwonums(dividend, divisor):
-    counter = 0
-    if dividend >= 2**31:
-        return 2**31 -1
-    elif dividend <= -2**31:
-        return 2**31 - 1
-    if dividend < 0  and divisor > 0:
-        while dividend < 0:
-            dividend += divisor
-            counter -= 1
-        return counter
-    elif dividend < 0 and divisor < 0:
-        while dividend < 0:
-            dividend -= divisor
-            counter += 1  
-        return counter 
-    elif dividend > 0 and divisor > 0:
-        while dividend > 0:
-            dividend -= divisor
-            counter += 1
-        return counter
-    elif dividend > 0 and divisor < 0:
-        while dividend > 0:
-            dividend += divisor
-            counter -= 1
-        return counter
-    elif dividend == 0:
-        return 0   
+    rv = len(range(0, dividend, divisor))
+    if (dividend < 0 and divisor > 0) or (dividend > 0 and divisor < 0):
+        return -rv
+    else:
+        return rv
 
 print(dividetwonums(dividend, divisor))
