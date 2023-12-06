32b42703bddb7c9339c8cde8b37e6ed5f278bc0f
GondorGitaros <csaszardaniel81@gmail.com>
2023-12-06 20:27:49 +0100

    worked on solution

--- current/0819_MostCommonWord.py
+++ current/0819_MostCommonWord.py
@@ -1,14 +1,26 @@
 def mostCommonWord(paragraph, banned):
-      paragraph = paragraph.replace(",", "").replace(".", "").replace("!", "").replace("?", "").replace("'", "").replace(";", "")
-      paragraph = paragraph.lower()
-      paragraph = list(paragraph.split(" "))
-      paragraph = [i for i in paragraph if i not in banned]
-      hm = {}
-      for i in paragraph:
+    paragraph = paragraph.replace(",", " ")
+    print(paragraph)
+    for i in range(len(paragraph)):
+        if paragraph[i] == " " and paragraph[i + 1] == " ":
+            paragraph[i] == ""
+    print(paragraph)
+    paragraph = list(paragraph.replace(".", "").replace("!", "").replace("?", "").replace("'", "").replace(";", "").lower().split(" "))
+    paragraph = [i for i in paragraph if i not in banned]
+    hm = {}
+    for i in paragraph:
         if i in hm:
-          hm[i] += 1
+            hm[i] += 1
         else:
-          hm[i] = 0
-      return hm
-      
-print(mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", ["hit"]))
+            hm[i] = 0
+    key_list = list(hm.keys())
+    hmli = sorted(hm.values())
+    couter = 0
+    for i in hm:
+        if hm[i] == hmli[-1]:
+            return key_list[couter]
+        couter += 1
+        
+    return hm
+
+print(mostCommonWord("a, a, a, a, b,b,b,c, c", ["a"]))
