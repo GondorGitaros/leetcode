74119a8c6f7987ad72321acdfd55d55731e38ac2
GondorGitaros <csaszardaniel81@gmail.com>
2024-02-12 22:59:40 +0100

    added to current

---
+++ current/1002_Find_Common_Characters.py
@@ -0,0 +1,10 @@
+def commonChars(words):
+    letters = []
+    d = {}
+    result = []
+    for i in words[0]:
+        letters.append(i)
+
+lista = ["bella","label","roller"]
+
+print(commonChars(lista))
