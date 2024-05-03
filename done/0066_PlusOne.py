1110b4298f73d13c12f0ccb331e7adea00e4fffd
Császár Dániel <csaszardaniel81@gmail.com>
2024-05-03 22:26:30 +0200

    update

--- done/0066_PlusOne.py
+++ done/0066_PlusOne.py
@@ -1,13 +1,12 @@
-def plusOne(digits):
-    n = ""
-    for num in digits:
-        n += str(num)
-    n = str(int(n) + 1)
-    digits = []
-    for num in n:
-        digits.append(int(num))
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = ""
+        for num in digits:
+            n += str(num)
+        n = str(int(n) + 1)
+        digits = []
+        for num in n:
+            digits.append(int(num))
 
-    return digits
-
-print(plusOne([4,3,2,1]))
-print(plusOne([9]))
+        return digits
+        
